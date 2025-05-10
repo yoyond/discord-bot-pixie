c9e961c265eeeefe497a36e1f1ab791c9e0065c1
Lohanne Fraga <lohanne.fraga.yakut@gmail.com>
2025-05-10 15:25:06 -0300

    dictionary update

--- dictionary/keywords.js
+++ dictionary/keywords.js
@@ -1,6 +1,43 @@
 module.exports = {
+
   "olá": "Olá! Como posso ajudar você? 👋",
-  "ajuda": "Claro! Digite `!ping` para testar ou me diga o que você precisa.",
-  "bot": "Sim, eu sou um bot feito com discord.js!",
-  "tchau": "Até mais! 👋"
+  "ola": "Olá! Como posso ajudar você? 👋",
+  "oi": "Oii! Como posso ajudar? 🤖🤖",
+  "salve": "Salve Salve! 😁",
+  "tchau": "Até mais! 👋",
+  "bye": "bye bye! 👋",
+  "até": "Até mais! 👋",
+  "Bom dia": "Bom dia ☀ vamos jogar juntos?",
+  "Boa noite": "Boa noite!",
+
+   "onde está o ip?": "O ip do servidor está na barra de canais disponivel o dia todo para membros. Não achou? Chame um staff para te ajudar!", 
+   "cade o ip do serve": "O ip do servidor está na barra de canais disponivel o dia todo para membros. Não achou? Chame um staff para te ajudar!", 
+   "cade o ip": "O ip do servidor está na barra de canais disponivel o dia todo para membros. Não achou? Chame um staff para te ajudar!", 
+   "onde vejo o ip": "O ip do servidor está na barra de canais disponivel o dia todo para membros. Não achou? Chame um staff para te ajudar!", 
+   "onde pego o ip do serve": "O ip do servidor está na barra de canais disponivel o dia todo para membros. Não achou? Chame um staff para te ajudar!", 
+   "onde pego o ip do serve?": "O ip do servidor está na barra de canais disponivel o dia todo para membros. Não achou? Chame um staff para te ajudar!", 
+   "como acho o ip": "O ip do servidor está na barra de canais disponivel o dia todo para membros. Não achou? Chame um staff para te ajudar!", 
+   "onde acho o ip": "O ip do servidor está na barra de canais disponivel o dia todo para membros. Não achou? Chame um staff para te ajudar!", 
+   "onde fica ip": "O ip do servidor está na barra de canais disponivel o dia todo para membros. Não achou? Chame um staff para te ajudar!", 
+
+
+   "como entra": "Para entrar no servidor você deve fazer a #✅➥whitelist caso ainda nao tenha feito. Posso te ajudar em algo mais? 😍", 
+   "como entra no serve": "Para entrar no servidor você deve fazer a #✅➥whitelist caso ainda nao tenha feito. Posso te ajudar em algo mais? 😍", 
+   "como entra no server": "Para entrar no servidor você deve fazer a #✅➥whitelist caso ainda nao tenha feito. Posso te ajudar em algo mais? 😍", 
+   "como faço para entra no serve": "Para entrar no servidor você deve fazer a #✅➥whitelist caso ainda nao tenha feito. Posso te ajudar em algo mais? 😍", 
+
+
+   "ja fiz whitelist": "Que bom! Agora é so aguardar, dentro de alguns minutos você será oficialmente membro! 🥳",
+   "whitelist feita!": "Que bom! Agora é so aguardar, dentro de alguns minutos você será oficialmente membro! 🥳",
+   "fiz whitelist": "Que bom! Agora é so aguardar, dentro de alguns minutos você será oficialmente membro! 🥳",
+   "WL feita": "Que bom! Agora é so aguardar, dentro de alguns minutos você será oficialmente membro! 🥳",
+   "fiz wl": "Que bom! Agora é so aguardar, dentro de alguns minutos você será oficialmente membro! 🥳",
+   "ja fiz wl": "Que bom! Agora é so aguardar, dentro de alguns minutos você será oficialmente membro! 🥳",
+   "ja fiz wt": "Que bom! Agora é so aguardar, dentro de alguns minutos você será oficialmente membro! 🥳",
+   "fiz wt": "Que bom! Agora é so aguardar, dentro de alguns minutos você será oficialmente membro! 🥳",
+   "feita wt": "Que bom! Agora é so aguardar, dentro de alguns minutos você será oficialmente membro! 🥳",
+   "ja fiz a wt": "Que bom! Agora é so aguardar, dentro de alguns minutos você será oficialmente membro! 🥳",
+
+
+
 };
